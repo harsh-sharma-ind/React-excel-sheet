006a4e1c986dc5b812281a5df2a7b0cafaa94abf
Harsh Sharma <harsh.sharma@siya.tech>
2023-03-30 18:08:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from 'react';
+
+import {
+  SpreadsheetComponent,
+  SheetsDirective,
+  SheetDirective,
+  RangesDirective,
+  RowsDirective,
+  RowDirective,
+  CellDirective,
+  CellsDirective,
+  RangeDirective,
+  ColumnsDirective,
+  ColumnDirective,
+} from '@syncfusion/ej2-react-spreadsheet';
+import { data } from './datasource';
+import "./App.css";
 
 function App() {
+  const spreadsheetRef = useRef(null);
+
+  const onCreated = () => {
+    spreadsheetRef.current.cellFormat({ fontWeight: 'bold', textAlign: 'center' }, 'A1:H1');
+    spreadsheetRef.current.cellFormat({ verticalAlign: 'middle' }, 'A1:H5');
+    spreadsheetRef.current.cellFormat({ textAlign: 'center' }, 'A2:B5');
+    spreadsheetRef.current.cellFormat({ textAlign: 'center' }, 'D2:D5');
+    // To wrap the cells from E2 to E5 range
+    spreadsheetRef.current.wrap('E2:E5');
+    // To unwrap the H3 cell
+    spreadsheetRef.current.wrap('H3', false);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+    <div>
+      <SpreadsheetComponent
+        ref={spreadsheetRef}
+        created={onCreated}
+        showFormulaBar={false}
+       isReadOnly={true}
+      >
+        <SheetsDirective >
+          <SheetDirective name={'Movie List'} >
+            <RowsDirective>
+              <RowDirective height={30}  />
+              <RowDirective>
+                <CellsDirective>
+                  <CellDirective index={7} wrap={true} />
+                </CellsDirective>
+              </RowDirective>
+              <RowDirective>
+                <CellsDirective>
+                  <CellDirective index={7} wrap={true} />
+                </CellsDirective>
+              </RowDirective>
+              <RowDirective>
+                <CellsDirective>
+                  <CellDirective index={7} wrap={true} />
+                </CellsDirective>
+              </RowDirective>
+              <RowDirective>
+                <CellsDirective>
+                  <CellDirective index={7} wrap={true} />
+                </CellsDirective>
+              </RowDirective>
+              </RowsDirective>
+            <RangesDirective>
+              <RangeDirective dataSource={data} />
+            </RangesDirective>
+            <ColumnsDirective>
+              <ColumnDirective width={100} index={1} />
+              <ColumnDirective width={140} />
+              <ColumnDirective width={90} />
+              <ColumnDirective width={150} />
+              <ColumnDirective width={120} />
+              <ColumnDirective width={90} />
+              <ColumnDirective width={180} />
+            </ColumnsDirective>
+          </SheetDirective>
+        </SheetsDirective>
+      </SpreadsheetComponent>
+    </div>
     </div>
   );
 }
